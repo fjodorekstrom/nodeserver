a519d7ca107e4c998d2c5f25dbf946f058db43dc
fjodorekstromjayway <fjodor.ekstrom@jayway.com>
2012-08-21 12:47:33 +0200

    first working server with requesthandlers for /, /start and /upload

---
+++ server.js
@@ -0,0 +1,24 @@
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+	function onRequest(request, response){
+		var pathname = url.parse(request.url).pathname;
+		if(pathname == '/favicon.ico'){
+			response.writeHead(404, {'Content-Type' : 'text/plain'});
+			response.end('not found');
+		} else {
+		console.log("Request for " + pathname + " received");
+
+		route(handle, pathname);
+
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World");
+		response.end();
+		}
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started");
+}
+exports.start = start;
